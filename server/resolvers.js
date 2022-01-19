e0a004a754a9944c1681dc3b9b94a6d36a1b3de6
Dmitri <dmitri.urbanovich@gmail.com>
2022-01-19 17:54:32 +0300

    add CRUD methods for todos

--- server/resolvers.js
+++ server/resolvers.js
@@ -8,6 +8,10 @@ const resolvers = {
       const todos = await Todo.find();
       return todos;
     },
+    getTodo: async (root, args) => {
+      const todo = await Todo.findById(args.id);
+      return todo;
+    },
   },
   Mutation: {
     addTodo: async (root, args) => {
@@ -16,8 +20,27 @@ const resolvers = {
         detail: args.detail,
         date: args.date,
       });
-      await newTodo.save()
-      return newTodo
+      await newTodo.save();
+      return newTodo;
+    },
+    deleteTodo: async (root, args) => {
+      await Todo.findByIdAndDelete(args.id);
+      return `The todo deleted successfully`;
+    },
+    updateTodo: async (root, args) => {
+      const { id, title, detail, date } = args;
+      const updatedTodo = {};
+      if (title !== undefined) {
+        updatedTodo.title = title;
+      }
+      if (detail !== undefined) {
+        updatedTodo.detail = detail;
+      }
+      if (date !== undefined) {
+        updatedTodo.date = date;
+      }
+      const todo = await Todo.findByIdAndUpdate(id, updatedTodo, { new: true });
+      return todo;
     },
   },
 };
--- server/typeDefs.js
+++ server/typeDefs.js
@@ -13,10 +13,13 @@ const typeDefs = gql`
   type Query {
     welcome: String
     getTodos: [Todo]
+    getTodo(id: ID): Todo
   }
 
   type Mutation {
     addTodo(title: String, detail: String, date: Date): Todo
+    deleteTodo(id: ID): String
+    updateTodo(id: ID, title: String, detail: String, date: Date): Todo 
   }
 `;
 
