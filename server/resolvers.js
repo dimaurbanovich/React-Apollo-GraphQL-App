c9a0bc2c47a825ec9f7e87cd72790c85c7e95bde
Dmitri <dmitri.urbanovich@gmail.com>
2022-01-19 14:23:30 +0300

    add conection to MongoDB

---
+++ server/resolvers.js
@@ -0,0 +1,25 @@
+import Todo from "./models/Todo.js";
+const resolvers = {
+  Query: {
+    welcome: () => {
+      return "Hello world!";
+    },
+    getTodos: async () => {
+      const todos = await Todo.find();
+      return todos;
+    },
+  },
+  Mutation: {
+    addTodo: async (root, args) => {
+      const newTodo = new Todo({
+        title: args.title,
+        detail: args.detail,
+        date: args.date,
+      });
+      await newTodo.save()
+      return newTodo
+    },
+  },
+};
+
+export default resolvers;
